84b9c00428ab1bc1f102f0144ec703f740357a5f
Venus Ku <venuswku@gmail.com>
2022-01-12 19:40:59 -0800

    check if location is selected & validate inputted date and time

--- src/components/UploadPic.js
+++ src/components/UploadPic.js
@@ -3,6 +3,7 @@ import { FormControl, FormHelperText, Button, InputLabel, Select, MenuItem, Text
 import { useNavigate } from "react-router-dom";
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import { isValid, parseJSON } from "date-fns";
 import DateAdapter from "@mui/lab/AdapterDateFns";
 import { LocalizationProvider, DatePicker, TimePicker } from "@mui/lab";
 
@@ -15,17 +16,56 @@ const UploadPic = () => {
   const [location, setLocation] = React.useState("");
   const [locationError, setLocationError] = React.useState(false);
   const dateTime = new Date();
+  const [dateError, setDateError] = React.useState(false);
+  const [timeError, setTimeError] = React.useState(false);
 
+  // Stores the user's chosen photos.
   const onImageChange = (event) => {
     setImages([...event.target.files]);
   };
 
+  // Stores the inputted location.
+  const onLocationChange = (event) => {
+    setLocation(event.target.value);
+    setLocationError(false);
+  };
+
+  // Validates the inputted date and time before storing it.
+  const onDateTimeChange = (inputDateTime, changeType) => {
+    if (!isValid(parseJSON(inputDateTime))) {
+      switch (changeType) {
+        case "date":
+          setDateError(true);
+          break;
+        case "time":
+          setTimeError(true);
+          break;
+        default:
+          break;
+      }
+    } else if (changeType === "date") {
+      dateTime.setMonth(inputDateTime.getMonth());
+      dateTime.setDate(inputDateTime.getDate());
+      dateTime.setFullYear(inputDateTime.getFullYear());
+      setDateError(false);
+    } else if (changeType === "time") {
+      dateTime.setTime(inputDateTime.getTime());
+      setTimeError(false);
+    }
+  };
+
+  // Submits the user's photo(s) and their inputted info.
   const submitForm = () => {
     let canSubmit = true;
 
     // On error, display error message(s) for the invalid form field(s).
     if (location.length === 0) {
       setLocationError(true);
+      canSubmit = false;
+    }
+    if (!isValid(parseJSON(dateTime))) {
+      setDateError(true);
+      setTimeError(true);
       canSubmit = false;
     }
 
@@ -46,45 +86,49 @@ const UploadPic = () => {
   return (
     <div className="centeredContent">
       <h1>Upload Your Photo</h1>
-      {/* Show preview of uploaded image(s). */}
-      {imageURLs.map((imgSrc, i) => <img src={imgSrc} alt={images[i].name} key={i} />)}
-      <Button variant="contained" endIcon={<PhotoCameraIcon />}>
+      <Button variant="contained" endIcon={<PhotoCameraIcon />} style={{margin: "0px 5px 15px"}}>
         Take Photo
       </Button>
-      <Button variant="contained" onClick={() => imageUploader.current.click()} endIcon={<InsertPhotoIcon />}>
+      <Button variant="contained" onClick={() => imageUploader.current.click()} endIcon={<InsertPhotoIcon />} style={{margin: "0px 5px 15px"}}>
         Choose Photo
         <input type="file" required multiple accept="image/*" onChange={onImageChange} ref={imageUploader} style={{display: "none"}} />
       </Button>
-      <FormControl fullWidth>
-        <InputLabel id="location-label" required>Location</InputLabel>
-        <Select
-          labelId="location-label"
-          id="select-location"
-          value={location}
-          label="Location"
-          onChange={(event) => setLocation(event.target.value)}
-        >
-          {scLocations.map((loc, i) => <MenuItem value={i} key={i}>{loc}</MenuItem>)}
-        </Select>
-        {locationError && <FormHelperText>Please select the location.</FormHelperText>}
+      {/* Show preview of uploaded image(s). */}
+      {imageURLs.map((imgSrc, i) => <img src={imgSrc} alt={images[i].name} key={i} id="uploadedImg" />)}
+      <FormControl fullWidth id="form">
+        <FormControl error={locationError ? true : false}>
+          <InputLabel id="location-label" required>Location</InputLabel>
+          <Select
+            labelId="location-label"
+            id="select-location"
+            value={location}
+            label="Location"
+            onChange={onLocationChange}
+          >
+            {scLocations.map((loc, i) => <MenuItem value={i} key={i}>{loc}</MenuItem>)}
+          </Select>
+          {locationError && <FormHelperText>Please select a location.</FormHelperText>}
+        </FormControl>
         <TextField id="name-input" label="Name" variant="outlined" />
         <LocalizationProvider dateAdapter={DateAdapter}>
-          <DatePicker
-            label="Date *"
-            value={dateTime}
-            onChange={(newDate) => {
-              dateTime.setMonth(newDate.getMonth());
-              dateTime.setDate(newDate.getDate());
-              dateTime.setFullYear(newDate.getFullYear());
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <TimePicker
-            label="Time *"
-            value={dateTime}
-            onChange={(newTime) => { dateTime.setTime(newTime.getTime()); }}
-            renderInput={(params) => <TextField {...params} />}
-          />
+          <FormControl error={dateError ? true : false}>
+            <DatePicker
+              label="Date *"
+              value={dateTime}
+              onChange={(newDate) => { onDateTimeChange(newDate, "date"); }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            {dateError && <FormHelperText>Please input a valid date.</FormHelperText>}
+          </FormControl>
+          <FormControl error={timeError ? true : false}>
+            <TimePicker
+              label="Time *"
+              value={dateTime}
+              onChange={(newTime) => { onDateTimeChange(newTime, "time"); }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            {timeError && <FormHelperText>Please input a valid time.</FormHelperText>}
+          </FormControl>
         </LocalizationProvider>
         <TextField
           id="comments-input"
