a7248d7ef3574844a83ad5ccdae6ac28b16036b4
Venus Ku <venuswku@gmail.com>
2022-01-18 22:08:04 -0800

    making homepage look slightly more decent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import { Button } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import BackgroundImg from "../images/NaturalBridgesSite1_Dec.04.2021_11_39_iphone7.jpeg";
 
 const Home = (props) => {
   const { scrollElement } = props;
@@ -27,19 +28,30 @@ const Home = (props) => {
   }, [scrollElement]);
 
   return (
-    <div className="leftAlignedContent">
-      <p>global citizen science project to capture our changing coastlines</p>
-      <Button variant="contained">
-        <Link to="/upload" className="button">Upload Your Photo</Link>
-      </Button>
-      <h1 ref={aboutRef}>About CoastSnap</h1>
-      <p>CoastSnap is a project that provides a low-cost method for mapping shoreline changes by crowdsourcing images that are uploaded from community members to social media. Numerous CoastSnap stations have already been successfully implemented in different countries such as Australia, France, the United Kingdom, Spain and Canada. Our project aims to engage people on coastal issues and the impacts that climate change will have on local communities throughout Santa Cruz in the United States. We currently are partnering with the United States Geological Survey (USGS), the City of Santa Cruz, and many other supportive local organizations to bring CoastSnap to Santa Cruz. Growing interest and participation in CoastSnap from the general public will allow an increased amount of public awareness on sustainable issues, as well as a larger set of data collection for concise and accurate information about rising sea levels.</p>
-      <Button variant="contained">
-        <a href="https://www.coastsnap.com/" target="_blank" rel="noopener noreferrer" className="button">Visit the CoastSnap Website</a>
-      </Button>
-      <h1 ref={locationsRef}>Santa Cruz Locations</h1>
-      <p>Natural Bridges Site 1</p>
-      <p>Natural Bridges Site 2</p>
+    <div>
+      <img className="backgroundImage" src={BackgroundImg} alt={"Natural Bridges Site 1"}/>
+      <div className="leftAlignedContent">
+        <p>global citizen science project to capture our changing coastlines</p>
+        <Stack direction="row" justifyContent="center" spacing={2}>
+          <Link to="/upload" className="button">
+            <Button variant="contained">Upload Your Photo</Button>
+          </Link>
+          <a href="https://play.google.com/store/apps/details?id=com.spotteron.coastsnap" target="_blank" rel="noopener noreferrer" className="button">
+            <Button variant="contained">Google Play</Button>
+          </a>
+          <a href="https://apps.apple.com/us/app/coastsnap-spotteron/id1529921850" target="_blank" rel="noopener noreferrer" className="button">
+            <Button variant="contained">App Store</Button>
+          </a>
+        </Stack>
+        <h1 ref={aboutRef}>About CoastSnap</h1>
+        <p>CoastSnap is a project that provides a low-cost method for mapping shoreline changes by crowdsourcing images that are uploaded from community members to social media. Numerous CoastSnap stations have already been successfully implemented in different countries such as Australia, France, the United Kingdom, Spain and Canada. Our project aims to engage people on coastal issues and the impacts that climate change will have on local communities throughout Santa Cruz in the United States. We currently are partnering with the United States Geological Survey (USGS), the City of Santa Cruz, and many other supportive local organizations to bring CoastSnap to Santa Cruz. Growing interest and participation in CoastSnap from the general public will allow an increased amount of public awareness on sustainable issues, as well as a larger set of data collection for concise and accurate information about rising sea levels.</p>
+        <Button variant="contained">
+          <a href="https://www.coastsnap.com/" target="_blank" rel="noopener noreferrer" className="button">Visit the CoastSnap Website</a>
+        </Button>
+        <h1 ref={locationsRef}>Santa Cruz Locations</h1>
+        <p>Natural Bridges Site 1</p>
+        <p>Natural Bridges Site 2</p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AppBar, Toolbar, CssBaseline, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
@@ -18,29 +18,43 @@ const useStyles = makeStyles((theme) => ({
   },
   link: {
     textDecoration: "none",
-    color: "white",
+    color: "rgba(255, 255, 255, 0.8)",
     fontSize: "20px",
     marginLeft: theme.spacing(5),
     "&:hover": {
-      color: "yellow",
+      color: "white",
       borderBottom: "1px solid white",
     },
+  },
+  active: {
+    color: "white",
+    textDecoration: "none",
+    borderBottom: "1px solid white",
   },
 }));
 
 const Navbar = (props) => {
   const { scrollTo } = props;
   const classes = useStyles();
+  const location = useLocation();
+
+  const atPath = (path) => {
+    if (location.pathname === path) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   return (
-    <AppBar position="static">
+    <AppBar position={atPath("/") ? "absolute" : "static"} color={atPath("/") ? "transparent": "primary"} elevation={0}>
       <CssBaseline />
       <Toolbar>
         <Typography variant="h4" className={classes.logo} onClick={() => scrollTo("")}>
           <Link to="/" className={classes.whiteLink}>CoastSnap</Link>
         </Typography>
         <div className={classes.navlinks}>
-          <Link to="/upload" className={classes.link}>Upload</Link>
+          <Link to="/upload" className={classes.link + " " + (atPath("/upload") ? classes.active : "")}>Upload</Link>
           <Link to="/" className={classes.link} onClick={() => scrollTo("about")}>About</Link>
           <Link to="/" className={classes.link} onClick={() => scrollTo("locations")}>Locations</Link>
         </div>
