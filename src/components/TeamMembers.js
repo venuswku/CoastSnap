cad81d4cfeac73fc58ca06348e9eaa64267d75b1
Venus Ku <venuswku@gmail.com>
2022-06-22 15:53:22 -0700

    add responsive styling to team members section

--- src/components/TeamMembers.js
+++ src/components/TeamMembers.js
@@ -1,27 +1,64 @@
 import React from "react";
+import { Grid, Tooltip } from "@mui/material";
 const team = require("../data/teamMembers.json");
 
 const TeamMembers = () => {
   return (
-    <div>
+    <Grid container spacing={2}>
       {team.map((member, i) =>
-        <div className="flexColumnCenter teammateWrapper" key={i}>
-          {member.image ?
-            <img src={require("../images/UCSC Team/" + member.image)} alt={member.name + "'s Profile Picture"} className="teammatePic" />
+        <Grid item xs={12} md={6}>
+          {member.link ?
+            <a href={member.link} target="_blank" rel="noopener noreferrer" className="teammateWrapper" key={i}>
+              {member.image ?
+                (member.name === "Litzia Galvan" ?
+                  <Tooltip title="Meg Mindlin Photography" placement="bottom">
+                    <img src={require("../images/UCSC Team/" + member.image)} alt={member.name + "'s Profile Picture"} className="teammatePic" />
+                  </Tooltip>
+                  :
+                  <img src={require("../images/UCSC Team/" + member.image)} alt={member.name + "'s Profile Picture"} className="teammatePic" />
+                )
+                :
+                <div className="teammateInitialsWrapper">
+                  <h1 className="teammateInitials">{member.initials}</h1>
+                </div>
+              }
+              <div className="marginLeft20">
+                <h2>{member.name}</h2>
+                <p className="grayText">
+                  {member.role}
+                  <br></br>
+                  {member.education}
+                </p>
+              </div>
+            </a>
             :
-            <div className="teammateInitialsWrapper">
-              <h1 className="teammateInitials">{member.initials}</h1>
+            <div className="teammateWrapper" key={i}>
+              {member.image ?
+                (member.name === "Litzia Galvan" ?
+                  <Tooltip title="Meg Mindlin Photography" placement="bottom">
+                    <img src={require("../images/UCSC Team/" + member.image)} alt={member.name + "'s Profile Picture"} className="teammatePic" />
+                  </Tooltip>
+                  :
+                  <img src={require("../images/UCSC Team/" + member.image)} alt={member.name + "'s Profile Picture"} className="teammatePic" />
+                )
+                :
+                <div className="teammateInitialsWrapper">
+                  <h1 className="teammateInitials">{member.initials}</h1>
+                </div>
+              }
+              <div className="marginLeft20">
+                <h2>{member.name}</h2>
+                <p className="grayText">
+                  {member.role}
+                  <br></br>
+                  {member.education}
+                </p>
+              </div>
             </div>
           }
-          <h2>{member.name}</h2>
-          <p className="grayText">
-            {member.role}
-            <br></br>
-            {member.education}
-          </p>
-        </div>
+        </Grid>
       )}
-    </div>
+    </Grid>
   );
 };
 
